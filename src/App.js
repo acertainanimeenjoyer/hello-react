8f5086f101d2037aaf1686598f2866c72308c38f
acertainanimeenjoyer <donamanh2015@gmail.com>
2025-06-11 18:47:13 +0700

    Moved React app to root

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World from React!</h1>
+      <p>✨ Deployed with Vercel — by a web sorcerer!</p>
+    </div>
+  );
+}
+
+export default App;
